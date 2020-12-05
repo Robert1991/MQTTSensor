3ca3dd6434f54a0f16ee22f424eb28420df62877
Robert Waddell <rwaddell91@googlemail.com>
2020-12-05 16:39:54 +0100

    added code for new former ikea solar light sensor actor

---
+++ MQTTFormerIkeaSolarLights/MQTTFormerIkeaSolarLights.ino
@@ -0,0 +1,43 @@
+#include <IOTClients.h>
+#include <MQTTDevices.h>
+#include "credentials.h"
+
+#define PHOTO_SENSOR_PIN A0
+
+WiFiClient espClient;
+MQTTClient client(750);
+MessageQueueClient* mqttClient = new MessageQueueClient(MQTT_CLIENT_NAME, MQTT_USERNAME, MQTT_PASSWORD);
+
+MQTTDeviceInfo deviceInfo = {"XT9Ohl" ,"former_ikea_light", "homeassistant", "Node MCU", "RoboTronix"};
+MQTTDevicePing* devicePing = new MQTTDevicePing(deviceInfo,"sjLB6Q", 30000);
+MQTTSwitch* transistorLight = new MQTTSwitch(deviceInfo, "ODQ7Jt", D3);
+MQTTPhotoLightSensor* photoSensor = new MQTTPhotoLightSensor(deviceInfo,"zMvxqy", PHOTO_SENSOR_PIN);
+
+MQTTDeviceService* mqttDeviceService = new MQTTDeviceService(mqttClient, 3, 1);
+
+void setup() {
+  //Serial.begin(9600);
+  setupWifiConnection(WIFI_SSID, WIFI_PASSWORD); 
+  
+  client.begin(MQTT_BROKER, MQTT_PORT, espClient);
+  client.onMessage(messageReceived);
+
+  mqttClient -> setupClient(&client);
+  //mqttClient -> setVerbose(true);
+
+  mqttDeviceService -> addPublisher(photoSensor);
+  mqttDeviceService -> addPublisher(devicePing);
+  mqttDeviceService -> addStateConsumer(transistorLight);
+  mqttDeviceService -> setupMQTTDevices();
+}
+
+void loop() {
+  checkWifiStatus(WIFI_SSID, WIFI_PASSWORD);
+  mqttDeviceService -> executeLoop();
+  delay(100);
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  mqttDeviceService -> handleMessage(topic, payload);
+}
