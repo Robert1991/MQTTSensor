3116ec4c8dbe06de4fd2582d66710290596a9195
Robert Waddell <rwaddell91@googlemail.com>
2020-12-09 18:12:00 +0100

    added device reset switch to MQTTBedroomSensor

--- MQTTBedroomSensor/MQTTBedroomSensor.ino
+++ MQTTBedroomSensor/MQTTBedroomSensor.ino
@@ -12,7 +12,9 @@ MQTTClient client(750);
 MessageQueueClient* mqttClient = new MessageQueueClient(MQTT_CLIENT_NAME, MQTT_USERNAME, MQTT_PASSWORD);
 
 MQTTDeviceInfo deviceInfo = {"yPx7nV" ,"bedroom_sensor_1", "homeassistant", "Node MCU D1 Mini", "RoboTronix"};
+MQTTDeviceResetSwitch* resetSwitch = new MQTTDeviceResetSwitch(deviceInfo, "CfagA2");  
 MQTTDevicePing* devicePing = new MQTTDevicePing(deviceInfo, "Q6YALb", 30000);
+
 MQTTPhotoLightSensor* photoSensor = new MQTTPhotoLightSensor(deviceInfo, "lPYQso", PHOTO_SENSOR_PIN);
 MQTTMotionSensor* motionSensor = new MQTTMotionSensor(deviceInfo, "ZcaJoY", MOTION_SENSOR_PIN, 10, 10, 4);
 
@@ -20,15 +22,21 @@ DHT dht(DHT_PIN, DHTTYPE);
 MQTTHumiditySensor* humiditySensor = new MQTTHumiditySensor(deviceInfo, &dht, "uLE9b9");
 MQTTTemperatureSensor* temperatureSensor = new MQTTTemperatureSensor(deviceInfo, &dht, "BOVhu3");
 
-MQTTDeviceService* mqttDeviceService = new MQTTDeviceService(mqttClient, 6, 1);
+MQTTDeviceService* mqttDeviceService = new MQTTDeviceService(mqttClient, 7, 2);
 
 void setup() {
+  // Turn off onboard led
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  
   setupWifiConnection(WIFI_SSID, WIFI_PASSWORD); 
   
   client.begin(MQTT_BROKER, MQTT_PORT, espClient);
   client.onMessage(messageReceived);
 
   mqttClient -> setupClient(&client);
+  
+  mqttDeviceService -> setResetStateConsumer(resetSwitch);
   
   mqttDeviceService -> addPublisher(devicePing);
   mqttDeviceService -> addPublisher(photoSensor);
