23cea7d7080cf0ac69a56a14bfc80f04de5bdf3f
Robert Waddell <rwaddell91@googlemail.com>
2020-12-09 18:13:05 +0100

    MQTTLivingRoomClosetPlatine is now also a smart configure device

--- MQTTLivingRoomClosetPlatine/MQTTLivingRoomClosetPlatine.ino
+++ MQTTLivingRoomClosetPlatine/MQTTLivingRoomClosetPlatine.ino
@@ -10,65 +10,50 @@
 #define LED_STRIP_GREEN_PIN D2
 #define LED_STRIP_BLUE_PIN D6
 
-char HUMIDITY_STATE_TOPIC[] = "living_room/closet_humidity";
-char TEMPERATURE_STATE_TOPIC[] = "living_room/closet_temperature";
-char MOTION_SENSOR_STATE_TOPIC[] = "living_room/closet_motion";
-
-char RGB_LIGHT_STATE_TOPIC[] = "living_room/closet_light/status";
-char RGB_LIGHT_SWITCH_SUBSCRIPTION[] = "living_room/closet_light/switch";
-char RGB_LIGHT_BRIGHTNESS_SUBSCRIPTION[] = "living_room/closet_light/brightness/set";
-char RGB_LIGHT_COLOR_SET_SUBSCRIPTION[] = "living_room/closet_light/color/set";
-char RGB_LIGHT_COLOR_SET_STATE_TOPIC[] = "living_room/closet_light/color/status";
-char RGB_LIGHT_BRIGHTNESS_STATE_TOPIC[] = "living_room/closet_light/brightness/status";
-RGBPins stripPins = {LED_STRIP_RED_PIN, LED_STRIP_GREEN_PIN, LED_STRIP_BLUE_PIN};
-MQTTRgbLightConfiguration stripConfig = {stripPins, RGB_LIGHT_STATE_TOPIC, RGB_LIGHT_SWITCH_SUBSCRIPTION, RGB_LIGHT_BRIGHTNESS_SUBSCRIPTION, RGB_LIGHT_BRIGHTNESS_STATE_TOPIC,
-  RGB_LIGHT_COLOR_SET_SUBSCRIPTION, RGB_LIGHT_COLOR_SET_STATE_TOPIC};
-MQTTRgbLight* rgbLedStrip = new MQTTRgbLight(stripConfig);
-
 WiFiClient espClient;
-PubSubClient client(espClient);
-MQTTClient* mqttClient = new MQTTClient(MQTT_CLIENT_NAME, MQTT_USERNAME, MQTT_PASSWORD);
+MQTTClient client(750);
 
-MQTTMotionSensor* motionSensor = new MQTTMotionSensor(mqttClient, MOTION_SENSOR_STATE_TOPIC, MOTION_SENSOR_PIN);
+MessageQueueClient* mqttClient = new MessageQueueClient(MQTT_CLIENT_NAME, MQTT_USERNAME, MQTT_PASSWORD);
+
+MQTTDeviceInfo deviceInfo = {"9EgmS4" ,"living_room_sensor_actor_1", "homeassistant", "Node MCU", "RoboTronix"};
+MQTTDevicePing* devicePing = new MQTTDevicePing(deviceInfo, "lnwoRM", 30000);
+MQTTMotionSensor* motionSensor = new MQTTMotionSensor(deviceInfo, "VM5ts3", MOTION_SENSOR_PIN, 15, 20, 9);
+
 DHT dht(DHT_PIN, DHTTYPE);
-MQTTDhtSensor* dhtSensor = new MQTTDhtSensor(mqttClient, &dht, TEMPERATURE_STATE_TOPIC, HUMIDITY_STATE_TOPIC);
+MQTTHumiditySensor* humiditySensor = new MQTTHumiditySensor(deviceInfo, &dht, "2KGqAK");
+MQTTTemperatureSensor* temperatureSensor = new MQTTTemperatureSensor(deviceInfo, &dht, "VxgFPM");
+
+RGBPins stripPins = {LED_STRIP_RED_PIN, LED_STRIP_GREEN_PIN, LED_STRIP_BLUE_PIN};
+MQTTRgbLight* rgbLight = new MQTTRgbLight(deviceInfo, "L3Pc5A", stripPins);
+
+MQTTDeviceService* mqttDeviceService = new MQTTDeviceService(mqttClient, 5, 1);
 
 void setup() {
-  Serial.begin(9600);
-  motionSensor -> setupSensor();
-  dhtSensor -> setupSensor();
-  rgbLedStrip -> setupActor(mqttClient);
+  //Serial.begin(9600);
   
-  setupWifiConnection(WIFI_SSID, WIFI_PASSWORD);
-  mqttClient -> setVerbose(true);  
-  mqttClient -> setupClient(&client, MQTT_BROKER, MQTT_PORT);
-  client.setCallback(callback);
+  setupWifiConnection(WIFI_SSID, WIFI_PASSWORD); 
+  client.begin(MQTT_BROKER, MQTT_PORT, espClient);
+  client.onMessage(messageReceived);
+  
+  //mqttClient -> setVerbose(true);
+  mqttClient -> setupClient(&client);
+  
+  mqttDeviceService -> addPublisher(devicePing);
+  mqttDeviceService -> addPublisher(motionSensor);
+  mqttDeviceService -> addPublisher(humiditySensor);
+  mqttDeviceService -> addPublisher(temperatureSensor);
+  mqttDeviceService -> addStateConsumer(rgbLight);
+  mqttDeviceService -> setupMQTTDevices();
 }
 
 void loop() {
   checkWifiStatus(WIFI_SSID, WIFI_PASSWORD);
-  if (mqttClient -> loopClient()) {
-    motionSensor -> publishMeasurement();
-    dhtSensor -> publishMeasurement();
-  } else {
-    dhtSensor -> reset();
-    motionSensor -> reset();
-  }
+  mqttDeviceService -> executeLoop();
   
-  delay(200);
+  delay(50);
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
-  String messageTemp;
-  String topicTemp(topic);
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-  rgbLedStrip -> consumeMessage(mqttClient, topicTemp, messageTemp);
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  mqttDeviceService -> handleMessage(topic, payload);
 }
